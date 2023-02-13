57f28ce70f5a38da483db6449e01e6cc86982600
Abbhinav Jayaraman <aj29927@utexas.edu>
2023-02-13 17:05:26 -0600

    test code for the buttons

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert('hello world')
+function change_quantity(name, increment) {
+    let quantity = document.getElementById(`${name}-quantity`);
+    if (quantity > 0) {
+        quantity.value += increment;
+    }
+}
+
+function order() {
+    alert(`Hello World`)
+}
+
+function clear() {
+    alert(`Goodbye World`)
+}
+
